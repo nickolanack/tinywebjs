7a0d95a0ae93d100d544073ab58b34cdd1a6b7a6
nickolanack <nickblackwell82@gmail.com>
2015-08-09 17:47:06 -0700

    just fail

--- test/test.js
+++ test/test.js
@@ -12,6 +12,7 @@ var server=new WebServer({port:8091});
 var req = require('http').request('http://localhost:8091', function(res) {
 	
 	console.log(res);
+	assert.fail('...');
 	
 }).on('error', function(e) {
   assert.fail(e.message);
